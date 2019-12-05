34d885baac3f0bf0589b5fb0b874efb4e2a1b8a5
Ivan Tusnolobov <ivan.tusnolobov@gmail.com>
2019-12-05 17:55:10 +0300

    style: add scrollbar styles for preview and editor

--- src/components/Editor/editor.styles.js
+++ src/components/Editor/editor.styles.js
@@ -17,4 +17,34 @@ export const Textarea = styled.textarea`
     box-shadow: 0 0 0 2px #46a1f4;
     outline: none;
   }
+
+  /* Scrollbar */
+  overscroll-behavior: contain;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+  scrollbar-width: thin;
+  scrollbar-color: rgba(0, 0, 0, 0.2);
+
+  &::-webkit-scrollbar {
+    height: 0.3rem;
+    width: 0.5rem;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb:vertical {
+    min-height: 1rem;
+  }
+
+  &::-webkit-scrollbar-thumb:horizontal {
+    min-width: 1rem;
+  }
 `;
--- src/components/Preview/preview.styles.js
+++ src/components/Preview/preview.styles.js
@@ -10,4 +10,34 @@ export const PreviewContainer = styled.article`
   overflow-x: hidden;
   overflow-y: auto;
   word-wrap: break-word;
+
+  /* Scrollbar */
+  overscroll-behavior: contain;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+  scrollbar-width: thin;
+  scrollbar-color: rgba(0, 0, 0, 0.2);
+
+  &::-webkit-scrollbar {
+    height: 0.3rem;
+    width: 0.5rem;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb:vertical {
+    min-height: 1rem;
+  }
+
+  &::-webkit-scrollbar-thumb:horizontal {
+    min-width: 1rem;
+  }
 `;
